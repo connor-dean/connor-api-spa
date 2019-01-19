69a40387f4131e6d7a7594eb59e41f1ff7fc0330
Connor Dean <connor.dean@washburn.edu>
2019-01-19 15:09:58 -0600

    Added temp API call and input form logic to change parent state.

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 
 export default class InputForm extends Component {
+  state = {
+    inputValue: ""
+  };
+
   render() {
     const { inputType, placeholder, onClick, buttonText } = this.props;
+
     return (
       <div>
         <input
           className={"form input-width"}
           input={inputType}
           placeholder={placeholder}
+          onChange={this.props.onChangeValue}
         />
         <button className={"btn btn-primary"} onClick={onClick}>
           {buttonText}
--- src/containers/SearchContainer.jsx
+++ src/containers/SearchContainer.jsx
@@ -2,12 +2,47 @@ import React, { Component } from "react";
 import InputForm from "../components/InputForm";
 
 export default class SearchContainer extends Component {
+  state = {
+    inputValue: ""
+  };
+
+  // Update state from change in child component
+  handleChangeValue = e => {
+    this.setState({ inputValue: e.target.value }, () => {
+      console.log("SearchContainer state ", this.state.inputValue);
+    });
+  };
+
+  // Temporary API call until I can get API key for MySportsFeed
+  callApi = zip => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?zip=" +
+        zip +
+        "&appid=6dfb9b4baf665d9ad09363be550e786f&units=imperial"
+    )
+      .then(result => {
+        return result.json();
+      })
+      .then(jsonResult => {
+        // Do something with the result
+        this.setState({
+          cityName: jsonResult.name,
+          cityTemp: jsonResult.main.temp
+        });
+        console.log(this.state.cityName);
+        console.log(this.state.cityTemp);
+      })
+      .catch(error => console.log(error));
+  };
+
   render() {
     return (
       <InputForm
         input={"text"}
         placeholder={"Enter a player's name..."}
         buttonText={"Submit"}
+        onClick={() => this.callApi(this.state.inputValue)}
+        onChangeValue={this.handleChangeValue}
       />
     );
   }
